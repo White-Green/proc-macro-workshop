d06d21979233637cf23533ea8b3e38640a4c1b03
White-Green <43771790+White-Green@users.noreply.github.com>
2021-07-30 10:06:36 +0900

    builder/02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,29 @@
 use proc_macro::TokenStream;
 
-use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
+use quote::{format_ident, quote};
+use syn::{Data, DeriveInput, Field, Fields, parse_macro_input};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
-    let expanded = quote! {};
+    let DeriveInput { attrs: _, vis, ident, generics, data } = parse_macro_input!(input as DeriveInput);
+    let data = if let Data::Struct(data) = data { data } else { return TokenStream::new(); };
+    let fields = if let Fields::Named(fields) = data.fields { fields } else { return TokenStream::new(); };
+    let field_names = fields.named.iter().map(|Field { ident, .. }| quote! {#ident});
+    let fields = fields.named.iter().map(|Field { attrs, vis, ident, ty, .. }| quote! {#(#attrs)* #vis #ident : Option<#ty>});
+    let builder_struct_name = format_ident!("{}Builder",&ident);
+
+    let expanded = quote! {
+        impl #generics #ident {
+            #vis fn builder()->#builder_struct_name #generics{
+                #builder_struct_name {
+                    #(#field_names: None),*
+                }
+            }
+        }
+
+        #vis struct #builder_struct_name #generics {
+            #(#fields),*
+        }
+    };
     TokenStream::from(expanded)
 }
