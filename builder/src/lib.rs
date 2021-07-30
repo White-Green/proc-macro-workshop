8696d239f68e4e1ac2b3c8989721e0c54fbd77bf
White-Green <43771790+White-Green@users.noreply.github.com>
2021-07-30 09:37:35 +0900

    builder/01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,11 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _input = parse_macro_input!(input as DeriveInput);
+    let expanded = quote! {};
+    TokenStream::from(expanded)
 }
