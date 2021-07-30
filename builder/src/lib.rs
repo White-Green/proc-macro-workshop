e9ced2916cd44c05f5d7122b3474b19d51a2de22
White-Green <43771790+White-Green@users.noreply.github.com>
2021-07-30 10:26:27 +0900

    builder/04

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,14 +8,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let DeriveInput { attrs: _, vis, ident, generics, data } = parse_macro_input!(input as DeriveInput);
     let data = if let Data::Struct(data) = data { data } else { return TokenStream::new(); };
     let fields = if let Fields::Named(fields) = data.fields { fields } else { return TokenStream::new(); };
-    let field_names = fields.named.iter().map(|Field { ident, .. }| quote! {#ident});
+    let field_names = fields.named.iter().map(|Field { ident, .. }| quote! {#ident}).collect::<Vec<_>>();
     let field_setters = fields.named.iter().map(|Field { ident, ty, .. }| quote! {
         fn #ident(&mut self, item: #ty)->&mut Self {
             self.#ident = Some(item);
             self
         }
-    });
-    let option_fields = fields.named.iter().map(|Field { attrs, vis, ident, ty, .. }| quote! {#(#attrs)* #vis #ident : Option<#ty>});
+    }).collect::<Vec<_>>();
+    let option_fields = fields.named.iter().map(|Field { attrs, vis, ident, ty, .. }| quote! {#(#attrs)* #vis #ident : Option<#ty>}).collect::<Vec<_>>();
+    let move_fields = fields.named.iter().map(|Field { ident, .. }| quote! {#ident: self.#ident.ok_or(Box::<dyn std::error::Error>::from(String::from("None")))}).collect::<Vec<_>>();
     let builder_struct_name = format_ident!("{}Builder",&ident);
 
     let expanded = quote! {
@@ -33,6 +34,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #generics #builder_struct_name {
             #(#vis #field_setters)*
+
+            #vis fn build(self)->Result<#ident, Box<dyn std::error::Error>>{
+                Ok(#ident{
+                    #(#move_fields?),*
+                })
+            }
         }
     };
     TokenStream::from(expanded)
